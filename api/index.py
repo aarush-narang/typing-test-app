fe449cd94b23b361392780ea2b04c6fe20e0e494
aarush-narang <aarushnarang@gmail.com>
2023-02-12 23:40:56 -0800

    move all content to api/

--- api/index.py
+++ api/index.py
@@ -1,11 +1,7 @@
 from flask import Flask, render_template
 from routes import main_routes, text_generator_routes
-import os
 
 app = Flask(__name__)
 
 app.register_blueprint(main_routes.main_router, url_prefix='/')
 app.register_blueprint(text_generator_routes.text_generator_router, url_prefix='/')
-
-if __name__ == '__main__':
-    app.run(host=os.getenv('DOMAIN'), port=os.getenv('PORT'), ssl_context=('../cert.pem', '../key.pem'), debug=True) # certificates for https
