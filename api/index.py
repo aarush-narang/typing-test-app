b3cd465740ae7f10dad9920d5d5e0f6cd0731620
aarush-narang <aarushnarang@gmail.com>
2023-02-12 23:43:04 -0800

    move all routes into api/index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
-from routes import main_routes, text_generator_routes
+from flask import Flask, render_template, jsonify
+import requests, random
 
 app = Flask(__name__)
 
-app.register_blueprint(main_routes.main_router, url_prefix='/')
-app.register_blueprint(text_generator_routes.text_generator_router, url_prefix='/')
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.get('/generateText')
+def generateText():
+    text = requests.get(f'http://metaphorpsum.com/paragraphs/1/{random.randint(3, 5)}').content.decode()
+    totalHorizontal = 0
+    totalLetters = 0
+    lineEndIndecies = []
+
+    words = text.split(' ')
+    for i in range(len(words)):
+        if(totalHorizontal + (len(words[i]))*13.85 > 800):
+            lineEndIndecies.append(totalLetters-1)
+            totalHorizontal = 0
+        totalHorizontal += (len(words[i])+1)*13.85
+        totalLetters += len(words[i])+1
+
+    return jsonify(text=text, indecies=lineEndIndecies)
